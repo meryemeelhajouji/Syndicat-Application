43e3bcbf673c36ea31faa541fda8ed5ea985e59e
meryemeelhajouji <meryemelhajouji.99@gmail.com>
2023-01-04 23:01:44 +0100

    add test unitaire de api clinet and appartement

--- server/test/authTest.test.js
+++ server/test/authTest.test.js
@@ -13,9 +13,79 @@ describe("auth", () => {
 
   it("login ", async () => {
     const res = await supertest(app).post("/api/auth/login").send({
-      email: "mery@gmail.com",
-      password: "12345678",
+      email: "tt@gmail.com",
+      password: "1234567890",
     });
     expect(res.statusCode).toEqual(200);
   });
+});
+
+//test client
+describe("client", () => {
+  it("Add ", async () => {
+    const res = await supertest(app).post("/api/client").send({
+      name: "tttt",
+      cin: "tt1234m",
+      tel: "1234567890",
+    });
+  });
+  expect(res.statusCode).toEqual(200);
+});
+it("Delete client", async () => {
+  const res = await supertest(app).delete(
+    "/api/client/63b1f04f028ba228eb945e02"
+  );
+  expect(res.statusCode).toEqual(400);
+});
+it("Modifier client", async () => {
+  const res = await supertest(app)
+    .patch("/api/client/63b1f04f028ba228eb945e02")
+    .send({
+      name: "tttt",
+      cin: "tt1234m",
+      tel: "1234567890",
+    });
+  expect(res.statusCode).toEqual(400);
+});
+it("Get client", async () => {
+  const res = await supertest(app).get("/api/client");
+  expect(res.statusCode).toEqual(400);
+});
+
+//test appartement
+describe("appartement", () => {
+  it("Add ", async () => {
+    const res = await supertest(app).post("/api/appartement").send({
+      adresse: "mmm",
+      numero: "123",
+      loue: "true",
+      surface: "1234567890",
+      prix: "1234567890",
+      client_id: "63b449b9ae3cab2ccbecb082",
+    });
+  });
+  expect(res.statusCode).toEqual(200);
+});
+it("Delete appartement", async () => {
+  const res = await supertest(app).delete(
+    "/api/appartement/63b1f04f028ba228eb945e02"
+  );
+  expect(res.statusCode).toEqual(400);
+});
+it("Modifier appartement", async () => {
+  const res = await supertest(app)
+    .patch("/api/appartement/63b1f04f028ba228eb945e02")
+    .send({
+      adresse: "mmm",
+      numero: "123",
+      loue: "true",
+      surface: "1234567890",
+      prix: "1234567890",
+      client_id: "63b449b9ae3cab2ccbecb082",
+    });
+  expect(res.statusCode).toEqual(400);
+});
+it("Get appartement", async () => {
+  const res = await supertest(app).get("/api/appartement");
+  expect(res.statusCode).toEqual(400);
 });
